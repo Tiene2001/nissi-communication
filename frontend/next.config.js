06f919308998a2fa504005793e01bf0c5ae0d577
Yoma Paul Cédric TIENE <cedrictiene@gmail.com>
2026-06-27 12:42:26 +0000

    fix: ignore eslint warnings during docker build

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     domains: ['localhost', 'api.nc.groupe-nissi.com', 'nc.groupe-nissi.com'],
   },
